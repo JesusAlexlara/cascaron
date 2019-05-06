89b238a0b6e175f911ce40a436883da67a5e60d6
Jesús Alejandro Lara Moreno <jesus_alexlm@alumnos.uaslp.edu.mx>
2019-05-06 09:58:13 -0500

    Se agrego lectura de video

--- sources/main.cpp
+++ sources/main.cpp
@@ -8,7 +8,8 @@ using namespace cv;
 
 int main( int argc, char** argv )
 {
-    VideoCapture cap(0);
+    //VideoCapture cap(0);
+    VideoCapture cap('video.mp4');
 
     Mat edges;
     namedWindow("edges",1);
@@ -20,10 +21,10 @@ int main( int argc, char** argv )
         Mat frame;
 
         cap >> frame;
-        cvtColor(frame, edges, COLOR_BGR2GRAY);
-        GaussianBlur(edges, edges, Size(15, 15), 2, 2);
-        Canny(edges, edges, 0, 30, 3);
-        imshow("edges", edges);
+        //cvtColor(frame, edges, COLOR_BGR2GRAY);
+        //GaussianBlur(edges, edges, Size(15, 15), 2, 2);
+        //Canny(edges, edges, 0, 30, 3);
+        imshow("video", edges);
         if(waitKey(30) >= 0) break;
     }
     return EXIT_SUCCESS;
