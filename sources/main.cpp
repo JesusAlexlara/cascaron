5c69e2ea6dec512047e34ec269e1b068bc86dc88
Jesús Alejandro Lara Moreno <jesus_alexlm@alumnos.uaslp.edu.mx>
2019-05-06 09:44:44 -0500

    Se actualizo el codigo fuente

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,4 +1,4 @@
-#include <opencv2/core/core.hpp>
+﻿#include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <iostream>
@@ -6,26 +6,58 @@
 using namespace std;
 using namespace cv;
 
+void convolucion(Mat &a, Mat &b, float ma[3][3]);
+
 int main( int argc, char** argv )
 {
-    VideoCapture cap(0);
+    Mat imagen;
+    Mat salida;
 
-    Mat edges;
-    namedWindow("edges",1);
+    imagen = imread("pp.jpeg", IMREAD_GRAYSCALE);
 
-    if(!cap.isOpened())
-        return EXIT_FAILURE;
+    salida = imagen.clone();
 
-    for (;;) {
-        Mat frame;
-
-        cap >> frame;
-        cvtColor(frame, edges, COLOR_BGR2GRAY);
-        GaussianBlur(edges, edges, Size(15, 15), 2, 2);
-        Canny(edges, edges, 0, 30, 3);
-        imshow("edges", edges);
-        if(waitKey(30) >= 0) break;
+    if(! imagen.data ) {
+        cout <<  "No se pudo abrir la imagen.." << endl ;
+        return -1;
     }
-    return EXIT_SUCCESS;
+
+    namedWindow("Imagen" , WINDOW_AUTOSIZE);
+    imshow("Imagen", imagen);
+
+    float ma[3][3] = {
+        {1/-1.f, 1/-1.f, 1/-1.f},
+        {1/-1.f, 1/8.f, 1/-1.f},
+        {1/-1.f, 1/-1.f, 1/-1.f}
+    };
+
+    convolucion(imagen, salida, ma);
+
+    namedWindow("Imagen salida" , WINDOW_AUTOSIZE);
+    imshow("Imagen salida",salida);
+
+    waitKey(0);
+
+    return 0;
+
+
 }
 
+void convolucion(Mat &a, Mat &b, float ma[3][3]) {
+    float sum;
+
+    for (int x = 1; x < a.rows - 1; x++) {
+        for (int y = 1 ; y < a.cols - 1; y++) {
+            sum = 0.0f;
+
+            for (int j = -1; j <= 1; j++) {
+                for (int k = -1; k <= 1; k++) {
+                    sum += ma[k+1][j+1] *
+                            a.at<uchar>(x - k, y - j);
+                }
+            }
+            b.at<uchar>(x,y) = sum;
+        }
+    }
+    return;
+}
